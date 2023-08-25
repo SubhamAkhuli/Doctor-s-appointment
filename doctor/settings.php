46959429e47321e53ddb33ca2790a17040146b08
Subham Akhuli <99539892+SubhamAkhuli@users.noreply.github.com>
2023-08-25 14:20:33 +0530

    Update settings.php
    Change in  edit option  Edit Your Details to Edit Your Account Details

--- doctor/settings.php
+++ doctor/settings.php
@@ -396,7 +396,7 @@
                                     </tr>
                                     <tr>
                                         <td>
-                                            <p style="padding: 0;margin: 0;text-align: left;font-size: 25px;font-weight: 500;">Edit Your Details. <br><br>
+                                            <p style="padding: 0;margin: 0;text-align: left;font-size: 25px;font-weight: 500;">Edit Your Account Details. <br><br>
                                         </td>
                                     </tr>
                                     <tr>
